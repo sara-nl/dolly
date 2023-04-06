ebd8217072170a88323894cfb91b04bd6f8fbf8b
robogast <robertjan.schlimbach@gmail.com>
2023-04-06 12:43:39 +0200

    small mod to install script

--- install-proj.sh
+++ install-proj.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-module load 2022 Python/3.11.2-GCCcore-12.2.0-bare
+module load 2022 Python/3.10.4-GCCcore-11.3.0-bare
 
 # Download PDM
 curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python3 -
